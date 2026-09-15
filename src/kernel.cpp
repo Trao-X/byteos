37825d013a6a57938fc5e3fd5cef2c91268e0d44
v3x <195168618+vthreex@users.noreply.github.com>
2026-09-15 07:05:22 +0200

    small changed thats not worth a new ver! reprofessionalizing

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,18 +1,18 @@
 #include <stdint.h>
-#include <stddef.h> // im only including this because the creator of the strcmp is a fuckign chud who uses null instead of nullptr
-// byteos 1.1.1 armv6-m stm32g0 for byteos development board
-// last update made it so no memory overflow when maxinput passed
+#include <stddef.h> // who uses null instead of nullptr
+// byteos 1.1.2 armv6-m stm32g0 for byteos development board
+// last update reprofessionalizing since i decided i was immature 2 days ago
 // credits: v3x, osdev, embeddedartistry.github.io
 
 
-volatile uint32_t* resetandcockcontrol_ioenablereg = (volatile uint32_t*)0x40021034;
-volatile uint32_t* resetandcockcontrol_advancedpenisbusenablereg = (volatile uint32_t*)0x4002103C;
-volatile uint32_t* youknowwhatgpiois_modereg = (volatile uint32_t*)0x50000000;
-volatile uint32_t* youknowwhatgpiois_altfunreglow = (volatile uint32_t*)0x50000020;
-volatile uint32_t* uartbutsynchronous2_ctrlgreg1yolookthatsaysgreg = (volatile uint32_t*)0x40004400;
-volatile uint32_t* uartbutsynchronous2_baudratereg = (volatile uint32_t*)0x4000440C;
-volatile uint32_t* uartbutsynchronous2_interuptstatusregimisspeledinterrupt = (volatile uint32_t*)0x4000441C;
-volatile uint32_t* uartbutsynchronous2_transmitdatareg = (volatile uint32_t*)0x40004428;
+volatile uint32_t* rcc_iopenr = (volatile uint32_t*)0x40021034;
+volatile uint32_t* rcc_apbenr1 = (volatile uint32_t*)0x4002103C;
+volatile uint32_t* gpio_moder = (volatile uint32_t*)0x50000000;
+volatile uint32_t* gpio_afrl = (volatile uint32_t*)0x50000020;
+volatile uint32_t* usart2_cr1 = (volatile uint32_t*)0x40004400;
+volatile uint32_t* usart2_brr = (volatile uint32_t*)0x4000440C;
+volatile uint32_t* usart2_isr = (volatile uint32_t*)0x4000441C;  // inter status register
+volatile uint32_t* usart2_tdr = (volatile uint32_t*)0x40004428; // transmit data register
 // that was a FUCKTON of copy and pasting the hex
 
 // strcmp from embeddedartistry.github.io
@@ -50,47 +50,49 @@ volatile uint32_t* uartbutsynchronous2_transmitdatareg = (volatile uint32_t*)0x4
      return r;
  }
 
+ // let me save here
+ // rcc = reset and clock control good good
+
 
 void uart_init() {
 
-    *resetandcockcontrol_ioenablereg |= (1 << 0);
-    *resetandcockcontrol_advancedpenisbusenablereg |= (1 << 17);
+    *rcc_iopenr |= (1 << 0); // reset and clock control io enable reg afaik
+    *rcc_apbenr1 |= (1 << 17); // rcc adv perihpal bus enable reg
 
-    *youknowwhatgpiois_modereg &= ~(3 << 4);
-    *youknowwhatgpiois_modereg |= (2 << 4);
+    *gpio_moder &= ~(3 << 4); // gpio mode register, i have fuckall idea what a mode register is
+    *gpio_moder |= (2 << 4);
 
-    *youknowwhatgpiois_altfunreglow |= (1 << 8);
+    *gpio_afrl |= (1 << 8); // gpio alternate low register, actually nvm alternate low FUNCTION
 
-    *uartbutsynchronous2_baudratereg = 139;
-    *uartbutsynchronous2_ctrlgreg1yolookthatsaysgreg |= (1 << 3) | (1 << 2) | (1 << 0);
-    *youknowwhatgpiois_modereg &= ~(3 << 6);
-    *youknowwhatgpiois_modereg |= (2 << 6);
-    *youknowwhatgpiois_altfunreglow |= (1 << 12); // uart init or smht idfk
+    *usart2_brr = 139; // uart with synchronous 2 baud rate register, prob something that has to do with uartttt
+    *usart2_cr1 |= (1 << 3) | (1 << 2) | (1 << 0); // gl to you in life if you are reading this, anyways control reg, bro my rename to propfessionalize naming was so fucking shit
+    *gpio_moder &= ~(3 << 6); // gpio mode register
+    *gpio_moder |= (2 << 6); // 2x
+    *gpio_afrl |= (1 << 12); // hm
 }
 
-// please tell me this is correct or im jumping off a bridge
-// it is, not today
+
 
 void pleaseputacharacter(char okillputacharacter) {
 // *uartbos = okillputacharacter; this is so fucking stupid and i like half understand it maybe idk (this is what i said before switching to the other chip and now i dont understand it at fucking all!)
-while(!(*uartbutsynchronous2_interuptstatusregimisspeledinterrupt & (1 << 7))) { } // this is probably something idk it sets bit :thumbsup: ok nvm it turns out ittt reads a bit and isolates bit 7 for testing
-*uartbutsynchronous2_transmitdatareg = okillputacharacter;
+while(!(*usart2_isr & (1 << 7))) { } // this is probably something idk it sets bit :thumbsup: ok nvm it turns out ittt reads a bit and isolates bit 7 for testing
+*usart2_tdr = okillputacharacter;
 }
 
 
-void putshitinuart(const char* okillputshitinuart) {
+void uartcharacterplacement(const char* okillplaceit) {
 
-    while (*okillputshitinuart != '\0') {
-    pleaseputacharacter(*okillputshitinuart);
-    okillputshitinuart++; // this took me like 10 fucking minutes to figure out + 5 for the braces
+    while (*okillplaceit != '\0') {
+    pleaseputacharacter(*okillplaceit);
+    okillplaceit++; // this took me like 10 fucking minutes to figure out + 5 for the braces
 }
 }
 
-volatile uint32_t* uartbutsynchronous2_givemefuckingadataregister = (volatile uint32_t*)0x40004424;
+volatile uint32_t* usart2_rdr = (volatile uint32_t*)0x40004424; // recieve data register
 
-char getmeafuckingcharacter() {
-while (!(*uartbutsynchronous2_interuptstatusregimisspeledinterrupt & (1 << 5))) {}
-return *uartbutsynchronous2_givemefuckingadataregister; // well no fucking shit
+char givcharacter() {
+while (!(*usart2_isr & (1 << 5))) {}
+return *usart2_rdr; // well no fucking shit
 
 // i kinda understand this maybe idk
 
@@ -101,47 +103,44 @@ int howmanycharacterscurrently = 0;
 
 
 void startanewterm() {
-putshitinuart("byteW>");
+uartcharacterplacement("byteW>");
 }
 
 
 
 void typepls() {
     while (1) {
-    char wtfdidutype = getmeafuckingcharacter();
-    if (wtfdidutype == '\n' || wtfdidutype == '\r') {
+    char userinputprobably = givcharacter();
+    if (userinputprobably == '\n' || userinputprobably == '\r') {
     maxinput[howmanycharacterscurrently] = '\0';
     howmanycharacterscurrently = 0;
     return;
 }
 if (howmanycharacterscurrently < 1023) {
-maxinput[howmanycharacterscurrently] = wtfdidutype;
+maxinput[howmanycharacterscurrently] = userinputprobably;
 howmanycharacterscurrently++;
-pleaseputacharacter(wtfdidutype);
+pleaseputacharacter(userinputprobably);
 }
 else {
-    putshitinuart("stop trying to overflow the kernel you fucking chud");
+    uartcharacterplacement("stop trying to overflow the kernel, you now may not use the terminal anymore");
 }
 }
 }
 extern "C" void boskernel() {  // copy pasted idk how to call c but yea ig its right maybe idk // its not copypasted anymore! i learned how to do basic c i guess...
 uart_init();
-putshitinuart("welcome to byteos 1.1.1 designed for byte dev board\r\n");
+uartcharacterplacement("welcome to byteos 1.1.2 designed for byte dev board\r\n");
 while (1) {
     startanewterm();
     typepls();
     if (strcmp(maxinput, "help") == 0) {
-        putshitinuart("\r\ngo fuck yourself im not helping you you fucking chud\r\n");
+        uartcharacterplacement("\r\nok so the current commands are: help\r\n");
     }
 }
 
-// the while(1) part is entirely copied because i do not understand c
-// kill yourselfffffffff took me like 30 mins of writing for 24 lines, have to order pcb v1 soon
+// the while(1) part is entirely copied because i do not understand c // this is not true anymore!
 // hi so im back and 50 lines and 30 more mins!
-// fuck c cpp better
 // we have added uart input
 // couple of hours for uart input, well not really probably like 30 mins again but like i split it many times
-// penis
 // i passed river in 0.5x speed, ignore this, 15.09.2026
 
 
